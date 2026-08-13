8d798089c7396b7ae4b0daaa097572c680c30423
Lucid dev <lawrencenjeri4@gmail.com>
2026-08-13 16:11:25 +0300

    Add Navbar component with navigation links

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <a href="/">Lucid Dev</a>
+
+      <div>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/channels">Channels</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
